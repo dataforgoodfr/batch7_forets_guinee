55dd2ab95a6a8b0d9a5b3e86387f48d29c209ea7
revel_sim <simon.revel@supelec.fr>
2020-06-03 12:05:38 +0200

    final update

--- flask_app/flask_server/transform_to_images.py
+++ flask_app/flask_server/transform_to_images.py
@@ -23,20 +23,20 @@ def convert_mask_to_image(img, hex0, hex1, hex2):
     return Image.fromarray(new_image.astype('uint8'), 'RGB')
 
 def convert_image_msi(img):
-    row = np.expand_dims(normalize_img(img[10,:,:]), axis = 2)
+    row = np.expand_dims(normalize_img(img[-1,:,:]), axis = 2)
     return Image.fromarray(np.uint8(255*np.concatenate((row, row, row), axis=2)), 'RGB')
 
 def convert_image_rgb(img):
-    f_row = np.expand_dims(normalize_img(img[0,:,:]), axis = 2)
-    s_row = np.expand_dims(normalize_img(img[1,:,:]), axis = 2)
-    t_row = np.expand_dims(normalize_img(img[2,:,:]), axis = 2)
-    return Image.fromarray(np.uint8(255*np.concatenate((f_row, s_row, t_row), axis=2)), 'RGB')
+    red_row = np.expand_dims(normalize_img(img[2,:,:]), axis = 2)
+    green_row = np.expand_dims(normalize_img(img[1,:,:]), axis = 2)
+    blue_row = np.expand_dims(normalize_img(img[0,:,:]), axis = 2)
+    return Image.fromarray(np.uint8(255*np.concatenate((red_row, green_row, blue_row), axis=2)), 'RGB')
 
 def convert_image_infra(img):
-    f_row = np.expand_dims(normalize_img(img[6,:,:]), axis = 2)
-    s_row = np.expand_dims(normalize_img(img[8,:,:]), axis = 2)
-    t_row = np.expand_dims(normalize_img(img[2,:,:]), axis = 2)
-    return Image.fromarray(np.uint8(255*np.concatenate((f_row, s_row, t_row), axis=2)), 'RGB')
+    red_row = np.expand_dims(normalize_img(img[6,:,:]), axis = 2)
+    green_row = np.expand_dims(normalize_img(img[8,:,:]), axis = 2)
+    blue_row = np.expand_dims(normalize_img(img[2,:,:]), axis = 2)
+    return Image.fromarray(np.uint8(255*np.concatenate((red_row, green_row, blue_row), axis=2)), 'RGB')
 
 def mix_images(im1, img2):
     im2 = img2.resize(im1.size)
